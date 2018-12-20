f8f4c2a3c4611a0cbb0fa8e6817f90eeb8f479df
Sean McBride <spmcbride1201@gmail.com>
2018-12-20 14:13:08 -0500

    feat: complete 1-14

---
+++ 1-tutorial-instruction/1-14/main.c
@@ -0,0 +1,34 @@
+/**
+ * K&R Exercise 1-13
+ * Page 24
+ * Write a program to print a histogram of the frequencies of different characters in its input.
+ * Completed 12/20/2018
+ **/
+
+#include <stdio.h>
+#define ASCII_LOWER 32
+#define ASCII_UPPER 126
+#define ASCII_RANGE 94
+
+int main(void)
+{
+    char c;
+    int frequencyOfASCIIChars[ASCII_RANGE] = {0};
+
+    while ((c = getchar()) != EOF)
+    {
+        frequencyOfASCIIChars[c - ASCII_LOWER]++;
+    }
+
+    for (int i = 0; i < sizeof(frequencyOfASCIIChars) / sizeof(frequencyOfASCIIChars[0]); i++)
+    {
+        putchar('\n');
+        printf("%c|", i + ASCII_LOWER);
+        for (int j = 0; j < frequencyOfASCIIChars[i]; j++)
+        {
+            putchar('*');
+        }
+    }
+    putchar('\n');
+    return 0;
+}
